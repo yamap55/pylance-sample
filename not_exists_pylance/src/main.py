44ba7b4bc6d49ccb82952c1a16f5d32e9840ce2f
yamap55 <tetuya120@hotmail.com>
2020-10-10 00:09:01 +0900

    [update] sample code

--- not_exists_pylance/src/main.py
+++ not_exists_pylance/src/main.py
@@ -1,6 +1,17 @@
-from typing import List
+import functools
 
 
-def hoge(s: str, i: int, l:List[str]):
-    functools
+def hoge(s: str, i: int, l: List[str]) -> None:
+    # 1. Listをインポート（ctrl + .）
+    # 2. randomをインポート
+    # 3. randomの候補（Intellicode）
+    pass
 
+
+# 4. 型のエラー
+hoge(1, 2, 3)
+
+
+# 5. 警告出してくれる
+# 6. 型を自分で設定すると補完してくれる
+a: str = huga()
