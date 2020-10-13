04d4f5de8fd2bfcb13a9a64fc9853854c44d554d
yamap55 <tetuya120@hotmail.com>
2020-10-13 16:59:37 +0900

    [update] pylance sample

--- exists_pylance/src/main.py
+++ exists_pylance/src/main.py
@@ -3,15 +3,16 @@ import functools
 
 def hoge(s: str, i: int, l: List[str]) -> None:
     # 1. Listをインポート（ctrl + .）
-    # 2. randomをインポート
-    # 3. randomの候補（Intellicode）
+    # 2. sysをインポート（sys.exit()）
+    # 3. randomをインポート
+    # 4. randomの候補（Intellicode）
     pass
 
 
-# 4. 型のエラー
+# 5. 型のエラー
 hoge(1, 2, 3)
 
 
-# 5. 警告出してくれる
-# 6. 型を自分で設定すると補完してくれる
+# 6. 警告出してくれる
+# 7. 型を自分で設定すると補完してくれる
 a: str = huga()
